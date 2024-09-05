937e01507635eeabc8d841fbb8e0b9e24223a325
danilo-oliv <daniloborges@unipam.edu.br>
2024-09-05 13:17:39 -0300

    feat: inicio responsividade

--- src/components/sobre/texto.js
+++ src/components/sobre/texto.js
@@ -6,15 +6,22 @@ import laboratorio from '../../assets/laboratorio.png'
 
 const Texto = () => {
     return(
-        <div className='texto'>
-            <p>Localizado no município de Patos de Minas, Minas Gerais, o laboratório ARCA surgiu do consenso entre técnicos e produtores sobre a necessidade de biotecnologias reprodutivas e pessoas capacitadas mais próximas às propriedades da região. Nosso foco é gerar valor aos nossos clientes, fornecendo um atendimento personalizado, a um valor justo, de modo a obter embriões de qualidade e altas taxas de concepção.
-            <br/>
-            ARCA EMBRIÕES, melhorando o AGORA.
-            </p>
+        <div className='texto-container'>
+            
+            <div className='texto'>
+                <p>Localizado no município de Patos de Minas, Minas Gerais, o laboratório ARCA surgiu do consenso entre técnicos e produtores sobre a necessidade de biotecnologias reprodutivas e pessoas capacitadas mais próximas às propriedades da região. Nosso foco é gerar valor aos nossos clientes, fornecendo um atendimento personalizado, a um valor justo, de modo a obter embriões de qualidade e altas taxas de concepção.
 
+                ARCA EMBRIÕES, melhorando o AGORA.
+                </p>
+            </div>
 
-            <img src={laboratorio} alt='Laboratório' width="750" height="750"/>
+            <div class="imagem">
+                <img src={laboratorio} alt='Laboratório' width="750" height="750"/>
+            </div>
+
         </div>
+
+
     )
 }
 
