485eb623e2cef4ea64a6479e6be1cc631403c479
lauanegs <130411983+lauanegs@users.noreply.github.com>
2024-09-09 20:35:39 +0000

    atualizando footer

--- src/components/equipe/Secao.js
+++ src/components/equipe/Secao.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './Secao.css'; // Certifique-se de que o caminho está correto
+import './Secao.css';
 
 const Secao = () => {
   return (
-    <div className="banner">
-      <h1>Nossos profissionais estão prontos para te atender!</h1>
-      <button>Entre em contato</button>
-    </div>
+    <section className="contact-banner">
+      <h2>Nossos profissionais estão prontos para te atender!</h2>
+      <div className="contact-buttons">
+        <a href="#contato" className="btn">Entre em Contato</a>
+      </div>
+    </section>
   );
 }
 
