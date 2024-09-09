496f390f69c687d0678aa1441b9f853718f335ec
lauanegs <130411983+lauanegs@users.noreply.github.com>
2024-09-09 20:46:06 +0000

    Alterações na página Equipe

--- src/components/equipe/Profissional.js
+++ src/components/equipe/Profissional.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import Profi from '../../assets/profissional.jpg';
 import './Profissional.css';
 
-const Profissional = ({ src, alt, nome, descricao }) => {
+const Profissional = () => {
   return (
-    <div className="profissional-item">
-      <img src={src} alt={alt} className="profissional-img" />
-      <div className="profissional-text">
-        <h3>{nome}</h3>
-        <p>{descricao}</p>
+    <section className="structure">
+      <h2>A melhor equipe</h2>
+      <p className="structure-subtitle">Contamos com profissionais altamente qualificados!</p>
+      <div className="structure-card-container">
+        <div className="structure-card">
+          <img src={Profi} alt="Incubadora" />
+          <h3>Profissional</h3>
+          <p className="structure-description">Formado em Medicina Veterinária.</p>
+        </div>
+        <div className="structure-card">
+          <img src={Profi} alt="Vitrificação" />
+          <h3>Profissional</h3>
+          <p className="structure-description">Formado em Medicina Veterinária.</p>
+        </div>
+        <div className="structure-card">
+          <img src={Profi} alt="Parcerias" />
+          <h3>Profissional</h3>
+          <p className="structure-description">Formado em Medicina Veterinária.</p>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/pages/Equipe.js
+++ src/pages/Equipe.js
@@ -3,60 +3,11 @@ import BannerEquipe from '../components/equipe/BannerEquipe';
 import Profissional from '../components/equipe/Profissional';
 import Secao from '../components/equipe/Secao';
 
-const profissionais = [
-  {
-    src: 'path/to/imagem1.jpg',
-    alt: 'Descrição da imagem 1',
-    nome: 'Profissional 1',
-    descricao: 'Descrição do profissional 1'
-  },
-  {
-    src: 'path/to/imagem2.jpg',
-    alt: 'Descrição da imagem 2',
-    nome: 'Profissional 2',
-    descricao: 'Descrição do profissional 2'
-  },
-  {
-    src: 'path/to/imagem3.jpg',
-    alt: 'Descrição da imagem 3',
-    nome: 'Profissional 3',
-    descricao: 'Descrição do profissional 3'
-  },
-  {
-    src: 'path/to/imagem4.jpg',
-    alt: 'Descrição da imagem 4',
-    nome: 'Profissional 4',
-    descricao: 'Descrição do profissional 4'
-  },
-  {
-    src: 'path/to/imagem5.jpg',
-    alt: 'Descrição da imagem 5',
-    nome: 'Profissional 5',
-    descricao: 'Descrição do profissional 5'
-  },
-  {
-    src: 'path/to/imagem6.jpg',
-    alt: 'Descrição da imagem 6',
-    nome: 'Profissional 6',
-    descricao: 'Descrição do profissional 6'
-  }
-];
-
 const Equipe = () => {
   return (
     <div>
       <BannerEquipe />
-      <div className="profissionais-container">
-        {profissionais.map((profissional, index) => (
-          <div className="profissional-item" key={index}>
-            <img src={profissional.src} alt={profissional.alt} className="profissional-img" />
-            <div className="profissional-text">
-              <h3>{profissional.nome}</h3>
-              <p>{profissional.descricao}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Profissional />
       <Secao />
     </div>
   );
