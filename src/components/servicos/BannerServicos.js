f3705a65e7cf5369d3c7f4dc2db2faf4f621ca8c
lauanegs <130411983+lauanegs@users.noreply.github.com>
2024-09-10 20:17:06 +0000

    Ajustes

---
+++ src/components/servicos/BannerServicos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BannerServicos.css'; // Certifique-se de que o caminho está correto
+
+const BannerServicos = () => {
+  return (
+    <div className="banner-servicos">
+      <h1>Conheça os nossos serviços!</h1>
+      <p>A qualidade dos nossos serviços leva a resultados, sempre acompanhados de um excelente atendimento. </p>
+      <button>Entre em contato</button>
+    </div>
+  );
+}
+
+export default BannerServicos;
